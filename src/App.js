be064d726c21b181118348c63319ba838c9a62b0
Joji-Jerone <100847982+Joji-Jerone@users.noreply.github.com>
2022-08-25 16:38:30 +0530

    components-classes

--- src/App.js
+++ src/App.js
@@ -24,7 +24,22 @@ export class Real_Madrid {
 const myfav = new Real_Madrid ("Cristiano Ronaldo" , "7" , "Portugal")
 myfav.Bio();
 
+class Real_Madrid_2022 extends Real_Madrid{
+  constructor (player_name , jersey_number , country , pos )
+  {
+    super(player_name);
+    super(jersey_number);
+    super(country);
+    this.position = pos;
+  }
 
+  Bio_2020(){
+    return this.Bio() + 'plays in the position of ' + this.position;
+  }
+}
+
+const myfav2020 = new Real_Madrid_2022 ("Cristiano Ronaldo" , "7" , "Portugal" , "Left wing Forward");
+myfav2020.Bio_2020();
 
 
 export default App;
