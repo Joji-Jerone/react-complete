30bbe36088ad43ea0cc1cf9200f40cc82185980f
Joji-Jerone <100847982+Joji-Jerone@users.noreply.github.com>
2022-08-25 18:02:14 +0530

    Arrow & destructuring

---
+++ src/components/arrow.js
@@ -0,0 +1,19 @@
+hello = function(){
+    return "Hello World"
+}
+
+hello1 = () => {
+    return "hello arrow"
+}
+
+class Manager{
+    constructor(name) {
+        this.name = name;
+        
+    }
+    
+     Records = () => {
+       const Coach = new Manager;
+
+     }
+}
---
+++ src/components/destructuring array.js
@@ -0,0 +1,49 @@
+//function destructuring
+
+function calculate(a, b) {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+  
+    return [add, subtract, multiply, divide];
+  }
+  
+  const [add, subtract, multiply, divide] = calculate(4, 7);
+
+  // object destructuring
+
+  const vehicleOne = {
+    brand: 'Ford',
+    model: 'Mustang',
+    type: 'car',
+    year: 2021, 
+    color: 'red'
+  }
+  
+  myVehicle(vehicleOne);
+  
+  function myVehicle({type, color, brand, model}) {
+    const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' + model + '.';
+  }
+
+// deeply nested destructing
+
+const vehicleOne = {
+    brand: 'Ford',
+    model: 'Mustang',
+    type: 'car',
+    year: 2021, 
+    color: 'red',
+    registration: {
+      city: 'Houston',
+      state: 'Texas',
+      country: 'USA'
+    }
+  }
+  
+  myVehicle(vehicleOne)
+  
+  function myVehicle({ model, registration: { state } }) {
+    const message = 'My ' + model + ' is registered in ' + state + '.';
+  }
